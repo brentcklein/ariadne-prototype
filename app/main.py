c9131988133ad9620abe91d68ff35acd19bb079d
Brent Klein <bklein@xometry.com>
2019-12-12 14:53:00 -0500

    WIP testing type checking

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from ariadne import gql, QueryType, make_executable_schema
 from ariadne.wsgi import GraphQL
+from graphql import GraphQLResolveInfo
+from typing import Union
 
 query = QueryType()
 
@@ -12,23 +14,33 @@ type_defs = gql("""
 """)
 
 
+class NotAString:
+    text: str
+
+    def __init__(self, text: str = '') -> None:
+        self.text = text
+
+    def __str__(self) -> str:
+        return self.text
+
+
 # Correct type returned
 @query.field("hello")
-def resolve_hello(*_):
-    return "heyo"
+def resolve_hello(_: Union[object, None], info: GraphQLResolveInfo) -> str:
+    user_agent = info.context["HTTP_USER_AGENT"]
+    return f"heyo, {user_agent}!"
 
 
 # Incorrect type returned. GraphQL returns error on method call
 @query.field("goodbye")
-def buhbye(*_):
+def buhbye(*_: Union[object, None]) -> dict:
     return {"I": "am not a string"}
 
 
 # Incorrect type returned. mypy sees no errors. GraphQL returns error on method call
 @query.field("farewell")
-def solong(*_) -> object:
-    result: object = {"I": "am an object"}
-    return result
+def solong(*_: Union[object, None]) -> NotAString:
+    return NotAString(text='Not a string tho')
 
 
 schema = make_executable_schema(type_defs, query)
