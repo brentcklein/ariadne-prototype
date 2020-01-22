aeaed25b61ab0372705370eba05d3f906ad9c01e
Brent Klein <bklein@xometry.com>
2020-01-22 17:35:00 -0500

    Moved schema to separate file

--- app/main.py
+++ app/main.py
@@ -1,70 +1,37 @@
-from ariadne import gql, QueryType, make_executable_schema, MutationType
+from os.path import dirname, join
+
+from ariadne import QueryType, MutationType
 from ariadne.asgi import GraphQL
+from ariadne_extensions import federation
 
 from app.status import get_current_status as get_current_status_from_mongo
 from app.status import set_current_status as set_current_status_in_mongo
 from app.user.index import get_user_first_name
 
+from graphql import GraphQLResolveInfo
+
+from typing import Optional, Dict
+
 query = QueryType()
 mutation = MutationType()
 
-type_defs = gql('''
-type Query {
-    """
-    Use strings like this to document your methods.
-    This description will automatically be reflected into the introspection schema for tooling.
-    This method will greet you by name if you're logged in, otherwise greet you as anonymous.
-    
-    If the user service is not available, this method will fail.
-    """
-    hello: String
-
-    echo(s: String): String
-
-    fail: String
-
-    """
-    Add two integers together.
-    """
-    add(a: Int!, b: Int!): Int!
-
-    """
-    The current world status, as determined by users. This may be null in normal operation.
-    """
-    currentStatus: String
-  }
-
-  input SetStatusInput {
-    status: String
-  }
-
-  type SetStatusPayload {
-    status: String
-  }
-
-  type Mutation {
-    """
-    Set the current world status. This method is only available to logged-in users.
-    """
-    setStatus(input: SetStatusInput!): SetStatusPayload
-  }
-''')
+# type_defs = load_schema_from_path(join(dirname(__file__), 'schema.graphql'))
 
 
-def addInts(a, b):
+def addInts(a: int, b: int) -> int:
     return a + b
 
 
 # Correct type returned
 @query.field("hello")
-def resolve_hello(parent, info):
+def resolve_hello(parent: Optional[object], info: GraphQLResolveInfo) -> str:
     name = get_user_first_name()
     name = name if name is not None else "anonymous person"
     return f"Hello, {name}, welcome to GraphQL!"
 
 
 @query.field("echo")
-def echo(*_, s=None):
+def echo(*_: Optional[object], s: str = None) -> str:
     if s is None or s == "":
         # Note that, as implemented, this is an anti-pattern. This will populate "errors" property, which should
         # be reserved for developer debugging information
@@ -73,28 +40,35 @@ def echo(*_, s=None):
 
 
 @query.field("fail")
-def fail(*_):
+def fail(*_: Optional[object]) -> None:
     raise ValueError("This API was destined for failure")
 
 
 @query.field("add")
-def add(*_, a, b):
+def add(*_: Optional[object], a: int, b: int) -> int:
     return addInts(a, b)
 
 
 @query.field("currentStatus")
-def current_status(*_):
+def current_status(*_: Optional[object]) -> str:
     # TODO: check info for a userId and raise an Exception if not
     return get_current_status_from_mongo()
 
 
 @mutation.field("setStatus")
-def set_current_status(*_, input):
+def set_current_status(*_: Optional[object], input: Dict[str, str]) -> Dict[str, str]:
     # TODO: check info for a userId and raise an Exception if not
     set_current_status_in_mongo(input["status"])
     return {"status": input["status"]}
 
 
-schema = make_executable_schema(type_defs, query, mutation)
+manager = federation.FederatedManager(
+    schema_sdl_file=join(dirname(__file__), 'schema.graphql'),
+    query=query
+)
+
+schema = manager.get_schema()
+
+# schema = make_executable_schema(type_defs, query, mutation)
 
 app = GraphQL(schema, debug=True)
